bf5a1ce635dfa653e96e8afda83e9017216312db
Gavin Panella <gavin.panella@canonical.com>
2012-03-13 17:15:33 +0000

    Bump version to 0.3.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from setuptools import (
 
 setup(
     name='txlongpoll',
-    version="0.2.12",
+    version="0.3",
     packages=find_packages('.') + ['twisted.plugins'],
     include_package_data=True,
     zip_safe=False,
