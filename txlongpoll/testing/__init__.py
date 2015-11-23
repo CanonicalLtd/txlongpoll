d77e6116b916e71400e7297057f54b52403cfaff
Free Ekanayaka <free.ekanayaka@canonical.com>
2015-11-23 10:02:27 +0100

    Take advantage of OptimisingTestSuite to make tests run faster and only delete queues as opposed to stopping and re-starting rabbitmq

--- txlongpoll/testing/__init__.py
+++ txlongpoll/testing/__init__.py
@@ -1,2 +1,10 @@
 # Copyright 2005-2011 Canonical Ltd.  This software is licensed under the
 # GNU Affero General Public License version 3 (see the file LICENSE).
+from testtools import run
+from testresources import OptimisingTestSuite
+
+# It seems there's no way to indicate the desired suite class to use via the
+# command line, so we set it in the module directly. Since this is the only
+# tweak we need, it's not worth creating a custom run() entry point and
+# instantiating a test runner/loader by hand.
+run.defaultTestLoaderCls.suiteClass = OptimisingTestSuite
