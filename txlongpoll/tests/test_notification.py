f68d8948a7610e777a723a2de46eddd1445badc5
Free Ekanayaka <free.ekanayaka@canonical.com>
2016-06-07 10:29:33 +0200

    Move FakeConnector to txlongpoll.testing.unit

--- txlongpoll/tests/test_notification.py
+++ txlongpoll/tests/test_notification.py
@@ -15,36 +15,15 @@ from testtools.twistedsupport import (
 
 from twisted.internet.task import Clock
 from twisted.logger import Logger
-from twisted.internet.address import IPv4Address
 
 from txamqp.factory import AMQFactory
-from txamqp.testing import AMQPump
 
 from txlongpoll.notification import (
     NotificationSource,
     Timeout,
     NotFound,
 )
-
-
-class FakeConnector(object):
-    """Return a client connected to a fake AMQPump transport."""
-
-    def __init__(self, factory, logger=None):
-        self.factory = factory
-        self.logger = logger
-        self.client = None  # Current client
-        self.transport = None  # Current transport
-
-    def __call__(self):
-        if self.client is None or self.client.closed:
-            address = IPv4Address("TCP", "127.0.0.1", 5672)
-            self.client = self.factory.buildProtocol(address)
-            self.transport = AMQPump(logger=self.logger)
-            self.transport.connect(self.client)
-
-        # AMQClient.channel() will fire synchronously here
-        return self.client.channel(1)
+from txlongpoll.testing.unit import FakeConnector
 
 
 class NotificationSourceTest(TestCase):
